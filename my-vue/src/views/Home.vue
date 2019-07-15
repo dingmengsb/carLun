1b5658018f857505040b9a0a308e3b28c55993b7
sbx <1604262993@qq.com>
2019-07-15 17:00:11 +0800

    框架

---
+++ my-vue/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+   <heAder><heAder>
+ </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import heAder from '@/components/home/heAder.vue'
+// import HelloWorld from '@/components/HelloWorld.vue';  @ is an alias to /src
+
+export default Vue.extend({
+  name: 'home',
+  components:{
+   heAder,
+  }
+});
+</script>
+
+<style>
+
+</style>
+
